97e2e1e8e3cd9ef44fce6fecb456409d6f393670
IguhSoares <iguhsoares@gmail.com>
2022-07-21 18:10:58 -0300

    ...

--- resize_images.py
+++ resize_images.py
@@ -1,10 +1,9 @@
 from PIL import Image, ImageOps
 
 import os
-from os import listdir
 from pathlib import Path
 
-import re # Regular Expressions Library
+import re
 import json
 import logging
 import argparse
@@ -12,25 +11,25 @@ import codecs
 
 
 def set_largest_dimension(input_message, error_message, try_again_message=False):
-    '''Asks user to enter the size of the resized image's largest dimension.
+    """Asks user to enter the size of the resized image's largest dimension.
 
-    If the input value is valid, returns the input value converted to a integer
+    If the input value is valid, returns the input value converted to an integer
     If not, shows error_message and
     asks for a different value, using try_again_message
 
     You may use '{input_value}' inside your error_message,
     and it will be replaced by the actual input_value entered by the user
-    '''
+    """
 
     input_value = input(input_message)
 
     # Using Regular Expressions to test whether the input value is valid
-    while not re.match('^\d+\s{0,1}(px){0,1}$', input_value):
+    while not re.match(r'^\d+\s?(px)?$', input_value):
         # input_value is not starting with one or more digits <^\d+>
         # followed by none or one white space <\s{0,1}>
         # and ending with one or none 'px' unit <(px){0,1}$>
         #
-        # Examples of valid inputs: '1200', '1200px'ou '1200 px'
+        # Examples of valid inputs: '1200', '1200px' or '1200 px'
         print(error_message.format(input_value=input_value))
 
         if not try_again_message:
@@ -44,25 +43,25 @@ def set_largest_dimension(input_message, error_message, try_again_message=False)
 
 
 def resize_images(images_dir, new_largest_dimension):
-    '''Iterates the images_dir applying new_largest_dimension to each image.
+    """Iterates the images_dir applying new_largest_dimension to each image.
     Returns the number of resized files.
 
     If original image's size is smaller than new_largest_dimension, the
-    image won't be resized and a info will be written to the log file.
+    image won't be resized and an info will be written to the log file.
 
     If a non image file is present in the images_dir, the file will be ignored
     and a warning will be written to the log file.
-    '''
+    """
     total_files_resized = 0
 
     for file_name in os.listdir(images_dir):
         file_path = f'{images_dir}/{file_name}'
 
         # Checks if it's not a directory and ignores .gitignore file
-        if os.path.isfile(file_path) and not file_name=='.gitignore':
+        if os.path.isfile(file_path) and not file_name == '.gitignore':
             try:
-                with Image.open(file_path) as image: # Context Manager
-                    image = ImageOps.exif_transpose(image) # Maintains orientation
+                with Image.open(file_path) as image:  # Context Manager
+                    image = ImageOps.exif_transpose(image)  # Maintains orientation
 
                     if new_largest_dimension > max(image.size):
                         logging.info(messages["file_not_resized"].format(
@@ -71,7 +70,7 @@ def resize_images(images_dir, new_largest_dimension):
                                         img_width=image.width,
                                         img_height=image.height
                                         )
-                                    )
+                                     )
                         something_in_log()
                     else:
                         # Resizes image's largest dimension
@@ -92,8 +91,8 @@ def resize_images(images_dir, new_largest_dimension):
 
 
 def get_languages(languages_dir):
-    '''Iterates languages_dir and returns an array with all the
-    languages available (E.g.: ['pt_BR', 'en_US', 'es_AR'])'''
+    """Iterates languages_dir and returns an array with all the
+    languages available (E.g.: ['pt_BR', 'en_US', 'es_AR'])"""
 
     json_files = list(Path(languages_dir).glob('*.json'))
 
@@ -106,7 +105,7 @@ def get_languages(languages_dir):
 
 
 def get_args():
-    '''Gets all the arguments passed in the command line'''
+    """Gets all the arguments passed in the command line"""
 
     parser = argparse.ArgumentParser(
                 formatter_class=argparse.ArgumentDefaultsHelpFormatter,
@@ -136,41 +135,46 @@ def get_args():
 
 
 def validate_args(args):
-    '''Validates all the arguments passed in the command line'''
+    """Validates all the arguments passed in the command line"""
 
+    # Validates language
     languages_dir = './language'
     languages = get_languages(languages_dir)
-    if args.language not in languages: # Validates language
+    if args.language not in languages:
         print(f'"{args.language}" is not available in {languages_dir}.\n' \
                 'Language will be set to "en_US".\n')
         args.language = 'en_US'
 
-    if not os.path.isdir(args.images_dir): # Validates images_dir
+    # Validates images_dir
+    if not os.path.isdir(args.images_dir):
         print(f'"{args.images_dir}" is not a valid directory.\n' \
                 f'Run {os.path.basename(__file__)} again and use --image_dir ' \
                 'to specify a valid directory.')
         exit()
 
-    if not os.path.isdir(args.resized_dir): # Validates resize_dir
+    # Validates resize_dir
+    if not os.path.isdir(args.resized_dir):
         print(f'"{args.resized_dir}" is not a valid directory.\n' \
                 f'Run {os.path.basename(__file__)} again and use --resized_dir ' \
                 'to specify a valid directory.')
         exit()
 
-    try: # Validates encoding
+    # Validates encoding
+    try:
         codecs.lookup(args.encoding)
     except LookupError as error:
         print(f'{error}. Using utf-8 instead.\n')
         args.encoding = 'utf-8'
 
-    if not Path(os.path.dirname(args.log_file)).exists(): # Validates log_file
+    # Validates log_file
+    if not Path(os.path.dirname(args.log_file)).exists():
         print(f'"{args.log_file}" is not a valid path. ' \
                 'Using "./log.txt" for log file instead.\n')
         args.log_file = 'log.txt'
 
 
 def something_in_log():
-    '''Sets True only the first time that something is written to log_file'''
+    """Sets True only the first time that something is written to log_file"""
 
     global log_has_something
     if not log_has_something:
@@ -188,12 +192,12 @@ with open("language/"+language+".json", "r", encoding=encoding) as json_file:
 
 log_file = args.log_file
 logging.basicConfig(
-            filename = log_file,
-            filemode = 'w',
-            encoding = encoding,
-            level = logging.INFO,
-            format = '%(asctime)s\n%(levelname)s: %(message)s\n',
-            datefmt = messages["date_format"]) # datefmt in language file
+            filename=log_file,
+            filemode='w',
+            encoding=encoding,
+            level=logging.INFO,
+            format='%(asctime)s\n%(levelname)s: %(message)s\n',
+            datefmt=messages["date_format"])  # datefmt in language file
 
 # Indicates that something has been written to log_file
 log_has_something = False
@@ -214,7 +218,7 @@ if total_files_resized == 1:
 else:
     final_message = messages["final_message"]["plural"]
     if total_files_resized > 1:
-            final_message += messages["saved_to"]["plural"]
+        final_message += messages["saved_to"]["plural"]
 
 
 print(final_message.format(
